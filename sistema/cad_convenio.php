0873d3b9a497fd1b7ad20b023e753ab1d00da48b
MaluhIvo <90100945+MaluhIvo@users.noreply.github.com>
2022-02-05 10:31:49 -0300

    sistema form

---
+++ sistema/cad_convenio.php
@@ -0,0 +1,16 @@
+<!--AQUI TEM HTML-->
+
+<form method="POST" action="sql_cad_convenio.php">
+
+ ===FORMULARIO===
+
+    </br></br>Nome:
+    <input type="text" name="frm_conv">
+
+    </br></br>Cooparticipação:
+    <input type="number" name="frm_coo">
+
+    </br></br><button type="submit">Enviar</button>
+
+</form>
+
